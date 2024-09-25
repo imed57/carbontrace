563604b3a9429bad13f6c265e7865c88a1227c31
Rayene Ben Torkia <115066360+rayenebnt@users.noreply.github.com>
2024-09-25 23:11:15 +0200

    index.js dans le dossier assets
    index.js dans le dossier assets

--- index.js
+++ index.js
@@ -1,165 +1,85 @@
-import React, { useState, useEffect, useRef } from 'react';
-import Image from 'next/image';
-import { useTheme } from 'next-themes';
+import heartOutline from './heart-outline.png';
+import heart from './heart.png';
+import logo from './logo.png';
+import search from './Search.png';
+import tick from './tick.png';
+import cross from './cross.png';
+import logo02 from './logo02.png';
+import menu from './menu.png';
+import discord from './discord.png';
+import instagram from './instagram.png';
+import telegram from './telegram.png';
+import twitter from './twitter.png';
+import upload from './upload.png';
+import nft1 from './nft1.png';
+import nft2 from './nft2.png';
+import nft3 from './nft3.png';
+import nft4 from './nft4.png';
+import nft5 from './nft5.png';
+import nft6 from './nft6.jpeg';
+import nft7 from './nft7.png';
+import nft8 from './nft8.png';
+import nft9 from './nft9.png';
+import nft10 from './nft10.jpg';
+import creator from './creator.png';
+import creator1 from './creator1.png';
+import creator2 from './creator2.png';
+import creator3 from './creator3.png';
+import creator4 from './creator4.png';
+import creator5 from './creator5.png';
+import creator6 from './creator6.png';
+import creator7 from './creator7.png';
+import creator8 from './creator8.png';
+import creator9 from './creator9.png';
+import creator10 from './creator10.jpg';
+import right from './right.png';
+import left from './left.png';
+import headerbg from './headerbg.png';
+import loader from './loader.gif';
+import arrow from './arrow.png';
+import logo_carbon from './logo_carbon.png';
+import Carbon from './Carbon.png';
 
-import images from '../assets';
-import { Banner, CreatorCard, NFTCard, SearchBar } from '../components';
-import { getCreators } from '../utils/getTopCreators';
-import { shortenAddress } from '../utils/shortenAddress';
-
-const Home = () => {
-  const [hideButtons, setHideButtons] = useState(false);
-  const [nfts, setNfts] = useState([]);
-  const [nftsCopy, setNftsCopy] = useState([]);
-  const [activeSelect, setActiveSelect] = useState('Recently added');
-  const parentRef = useRef(null);
-  const scrollRef = useRef(null);
-  const { theme } = useTheme();
-
-  useEffect(() => {
-    // Simulating the fetching of NFTs with static data
-    const fetchedNFTs = [
-      { tokenId: 1, name: "NFT 1", price: 1.2 },
-      { tokenId: 2, name: "NFT 2", price: 0.8 },
-      { tokenId: 3, name: "NFT 3", price: 2.3 },
-    ];
-    setNfts(fetchedNFTs);
-    setNftsCopy(fetchedNFTs);
-  }, []);
-
-  const handleScroll = (direction) => {
-    const { current } = scrollRef;
-    const scrollAmount = window.innerWidth > 1800 ? 270 : 210;
-
-    if (direction === 'left') {
-      current.scrollLeft -= scrollAmount;
-    } else {
-      current.scrollLeft += scrollAmount;
-    }
-  };
-
-  const isScrollable = () => {
-    const { current } = scrollRef;
-    const { current: parent } = parentRef;
-
-    if (current?.scrollWidth >= parent?.offsetWidth) {
-      setHideButtons(false);
-    } else {
-      setHideButtons(true);
-    }
-  };
-
-  useEffect(() => {
-    isScrollable();
-    window.addEventListener('resize', isScrollable);
-    return () => {
-      window.removeEventListener('resize', isScrollable);
-    };
-  }, []);
-
-  useEffect(() => {
-    const sortedNfts = [...nfts];
-    switch (activeSelect) {
-      case 'Price (low to high)':
-        setNfts(sortedNfts.sort((a, b) => a.price - b.price));
-        break;
-      case 'Price (high to low)':
-        setNfts(sortedNfts.sort((a, b) => b.price - a.price));
-        break;
-      case 'Recently added':
-        setNfts(sortedNfts.sort((a, b) => b.tokenId - a.tokenId));
-        break;
-      default:
-        setNfts(nfts);
-        break;
-    }
-  }, [activeSelect]);
-
-  const onHandleSearch = (value) => {
-    const filteredNfts = nfts.filter(({ name }) =>
-      name.toLowerCase().includes(value.toLowerCase())
-    );
-
-    if (filteredNfts.length) {
-      setNfts(filteredNfts);
-    } else {
-      setNfts(nftsCopy);
-    }
-  };
-
-  const onClearSearch = () => {
-    if (nfts.length && nftsCopy.length) {
-      setNfts(nftsCopy);
-    }
-  };
-
-  const topCreators = getCreators(nftsCopy);
-
-  return (
-    <div className="flex justify-center sm:px-4 p-12 bg-custom-dark-green">
-      <div className="w-full minmd:w-4/5">
-        <Banner
-          name="Discover, collect, and sell extraordinary NFTs"
-          childStyles="md:text-4xl sm:text-2xl xs:text-xl text-left"
-          parentStyles="justifyStart mb-6 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
-        />
-        {!nfts.length ? (
-          <h1 className="font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold ml-4 xs:ml-0">
-            That's weird... No NFTs for sale!
-          </h1>
-        ) : (
-          <>
-            <div className="">
-              <h1 className="font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold ml-4 xs:ml-0">
-                Top Sellers
-              </h1>
-              <div ref={parentRef} className="relative flex-1 max-w-full flex mt-3">
-                <div ref={scrollRef} className="flex flex-row w-max overflow-x-scroll no-scrollbar select-none">
-                  {topCreators.map((creator, i) => (
-                    <CreatorCard
-                      key={creator.seller}
-                      rank={i + 1}
-                      creatorImg={images[`creator${i + 1}`]}
-                      creatorName={shortenAddress(creator.seller)}
-                      creatorEths={creator.sum}
-                    />
-                  ))}
-                  {!hideButtons && (
-                    <>
-                      <div onClick={() => handleScroll('left')} className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 left-0 cursor-pointer">
-                        <Image src={images.left} layout="fill" objectFit="contain" alt="leftArrow" className={theme === 'light' ? 'filter invert' : ''} />
-                      </div>
-                      <div onClick={() => handleScroll('right')} className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 right-0 cursor-pointer">
-                        <Image src={images.right} layout="fill" objectFit="contain" alt="leftArrow" className={theme === 'light' ? 'filter invert' : ''} />
-                      </div>
-                    </>
-                  )}
-                </div>
-              </div>
-            </div>
-            <div className="mt-10">
-              <div className="flexBetween mx-4 sm:mx-0 minlg:mx-8 sm:flex-col sm:items-start">
-                <h1 className="flex-1 font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold sm:mb-4">
-                  Hot NFTs
-                </h1>
-                <div className="flex-2 sm:w-full flex flex-row sm:flex-col">
-                  <SearchBar
-                    activeSelect={activeSelect}
-                    setActiveSelect={setActiveSelect}
-                    handleSearch={onHandleSearch}
-                    clearSearch={onClearSearch}
-                  />
-                </div>
-              </div>
-              <div className="mt-3 w-full flex flex-wrap justify-start md:justify-center">
-                {nfts.map((nft) => <NFTCard key={nft.tokenId} nft={nft} />)}
-              </div>
-            </div>
-          </>
-        )}
-      </div>
-    </div>
-  );
+export default {
+  Carbon,
+  logo_carbon,
+  heartOutline,
+  heart,
+  logo,
+  search,
+  tick,
+  cross,
+  logo02,
+  menu,
+  discord,
+  instagram,
+  telegram,
+  twitter,
+  upload,
+  nft1,
+  nft2,
+  nft3,
+  nft4,
+  nft5,
+  nft6,
+  nft7,
+  nft8,
+  nft9,
+  nft10,
+  creator,
+  creator1,
+  creator2,
+  creator3,
+  creator4,
+  creator5,
+  creator6,
+  creator7,
+  creator8,
+  creator9,
+  creator10,
+  right,
+  left,
+  headerbg,
+  loader,
+  arrow,
 };
-
-export default Home;
